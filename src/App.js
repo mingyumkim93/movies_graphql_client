655a6b765333c1e2d01ce3e14b6981f11050df4e
mingyumkim93 <mingyumkim93@gmail.com>
2019-11-27 10:53:04 +0200

    setting up apollo client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import { HashRouter as Router, Route } from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+import client from "./apolloClient";
+import Home from "./Home";
+import Detail from "./Detail"
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <Route exact={true} path={"/"} component={Home}></Route>
+        <Route path={"/details/:movieId"} component={Detail}></Route>
+      </Router>
+    </ApolloProvider>
+
   );
 }
 
---
+++ src/Detail.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Detail = () => <div>Home</div>
+
+export default Detail;
+
