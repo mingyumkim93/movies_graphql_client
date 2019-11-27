6f435a799e041be648569de7fda3095f0852c731
mingyumkim93 <mingyumkim93@gmail.com>
2019-11-27 13:45:56 +0200

    adding css and styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Route } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
 import client from "./apolloClient";
 import Home from "./Home";
 import Detail from "./Detail"
@@ -8,12 +9,15 @@ import Detail from "./Detail"
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <Route exact={true} path={"/"} component={Home}></Route>
-        <Route path={"/details/:movieId"} component={Detail}></Route>
-      </Router>
-    </ApolloProvider>
-
+        <ApolloHooksProvider client={client}>
+          <Router>
+            <main>
+              <Route exact={true} path={"/"} component={Home} />
+              <Route path={"/details/:movieId"} component={Detail} />
+            </main>
+          </Router>
+        </ApolloHooksProvider>
+      </ApolloProvider>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,40 @@
 import React from "react";
-import { Query } from "react-apollo";
+import styled from "styled-components";
+import Movie from "./Movie";
+import { useQuery } from "react-apollo-hooks";
 import { HOME_PAGE } from "./queries";
+import { Helmet } from "react-helmet";
 
-const Home = () => <Query query={HOME_PAGE}>
-    {({loading, data, error}) => {
-        if(loading) return <span>loading</span>;
-        if(error) return <span>Something happened</span>
-    return data.movies.map(movie => <h2 key={movie.id}>{movie.title} / {movie.rating}</h2>)
-    }}
-    </Query>
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 0.7fr);
+  flex-wrap: wrap;
+  justify-items: center;
+`;
 
-export default Home;
-
+const Home = () => {
+    const { data, error, loading } = useQuery(HOME_PAGE);
+    return (
+      <Container>
+        <Helmet>
+          <title>Home | MovieQL</title>
+        </Helmet>
+        {loading && "Loading"}
+        {error && "Something is wrong"}
+        {!loading &&
+          data &&
+          data.movies &&
+          data.movies.map(movie => (
+            <Movie
+              id={movie.id}
+              key={movie.id}
+              poster={movie.medium_cover_image}
+              title={movie.title}
+              rating={movie.rating}
+            />
+          ))}
+      </Container>
+    );
+  };
+  
+  export default Home;
